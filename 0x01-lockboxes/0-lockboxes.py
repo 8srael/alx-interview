1887cb2ef9f51aad5a8a87b1584166160f04a90d
8srael <kouassiisrael36@gmail.com>
2023-08-04 00:10:22 +0000

    Task 0 ,only task, lockboxes✅

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" 
+    A module for working with lockboxes.
+"""
+def canUnlockAll3(boxes):
+    """Method that determines if all the boxes can be opened"""
+    def exploreBox(boxIndex, visited):
+        """Explore a box and mark all the boxes that can be opened"""
+        if visited[boxIndex]:
+            return 
+        visited[boxIndex] = True
+        
+        for key in boxes[boxIndex]:
+            if 0 <= key < len(boxes):
+                exploreBox(key, visited)
+
+    n = len(boxes)
+    visited = [False] * n
+    exploreBox(0, visited)
+    
+    return all(visited)
