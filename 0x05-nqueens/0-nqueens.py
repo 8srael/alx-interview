3f1a92e0cb5951b7c4293a940cc83f52c6b62e5c
8srael <kouassiisrael36@gmail.com>
2023-08-31 04:14:47 +0000

    Task 0. pycodestyle🧹

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -48,6 +48,7 @@ if __name__ == "__main__":
     if int(sys.argv[1]) < 4:
         print("N must be at least 4")
         exit(1)
+
     solutions = solveNQueens(int(sys.argv[1]))
     for case in solutions:
         print(case)
