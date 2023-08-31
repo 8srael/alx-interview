c6bccf70d9fd3f79393e3a02b7438a219f07afd0
8srael <kouassiisrael36@gmail.com>
2023-08-31 04:05:05 +0000

    Task 0. pycode style fixing

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -13,18 +13,17 @@ def solveNQueens(n: int) -> List[List[str]]:
     col = set()
     posDiag = set()
     negDiag = set()
-    
     res = []
     board = [['.'] * n for _ in range(n)]
 
     def backtrack(r):
         if r == n:
-            res.append([[r,c] for r in range(n) for c in range(n) \
-                if board[r][c] == 'Q'])
+            res.append([[r, c] for r in range(n) for c in range(n) \
+                    if board[r][c] == 'Q'])
             return
         for c in range(n):
             if c in col or (r + c) in posDiag or (r - c) in negDiag:
-                continue 
+                continue
             col.add(c)
             posDiag.add(r + c)
             negDiag.add(r - c)
@@ -50,8 +49,6 @@ if __name__ == "__main__":
     if int(sys.argv[1]) < 4:
         print("N must be at least 4")
         exit(1)
-
     solutions = solveNQueens(int(sys.argv[1]))
-    
     for case in solutions:
         print(case)
