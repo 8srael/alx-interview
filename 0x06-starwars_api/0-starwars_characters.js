82f9ab1764dd2fdd31faed6bab93c9fcf751535e
8srael <kouassiisrael36@gmail.com>
2023-09-05 17:30:26 +0000

    Task 0 completed✅

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+request.get(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+
+    for (const character of characters) {
+      request.get(character, function (error, _, body) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
+  }
+});
