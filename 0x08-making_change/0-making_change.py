bef83313314ce77b3d77a6b35be67aa30088b0af
8srael <kouassiisrael36@gmail.com>
2023-09-22 02:25:23 +0000

    updated

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,16 +4,18 @@
 
 
 def makeChange(coins, total):
-    """
-        determines the fewest number of coins needed
-        to meet a given amount total.
-    """
-    dp = [float('inf')] * (total + 1)
+    """ Create table and initialize"""
+    dp = [float('inf')]*(total + 1)
+
+    # number of coins to have 0
     dp[0] = 0
 
+    # boucle from 1 to total
     for i in range(1, total + 1):
-        for c in coins:
-            if i - c >= 0:
-                dp[i] = min(dp[i], 1 + dp[i - c])
+        # browse all coins
+        for coin in coins:
+            if coin <= i:
+                dp[i] = min(dp[i], dp[i - coin] + 1)
 
     return dp[total] if dp[total] != float('inf') else -1
+
