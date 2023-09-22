2291944c3a3c49fd92af25e9f69e62f7ddb43700
8srael <kouassiisrael36@gmail.com>
2023-09-22 02:05:15 +0000

    Task 0 finished✅

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+""" Task 0. Change comes from within"""
+
+def makeChange(coins, total):
+    """
+        determines the fewest number of coins needed
+        to meet a given amount total.
+    """
+    dp = [total + 1] * (total + 1)
+    dp[0] = 0
+
+    for i in range(1, total + 1):
+        for c in coins:
+            if i - c >= 0:
+                dp[i] = min(dp[i], 1 + dp[i -c])
+    return dp[total] if dp[total] != total + 1 else -1 
