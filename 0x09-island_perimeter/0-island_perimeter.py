0a4f0f8044a5c14789ac2fcbb291174036a4a803
8srael <kouassiisrael36@gmail.com>
2023-09-28 19:37:44 +0000

    🆗

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -7,21 +7,23 @@ def island_perimeter(grid):
     """
         Returns the perimeter of the island described in grid
     """
-    visited = set()
     row = len(grid)
     col = len(grid[0])
-
+        
+    visited = [[0 for x in range(col)] for y in range(row)]
+        
     def dfs(i, j):
-        """depth first search method recursive method"""
-        if i < 0 or j < 0 or i >= row or j >= col or grid[i][j] == 0:
+        """Depth first search method"""
+        if i < 0 or i > row-1 or j < 0 or j > col-1 or grid[i][j] == 0:
             return 1
-        if (i, j) in visited:
+        
+        if visited[i][j]:
             return 0
+        visited[i][j] = 1
+        
+        return dfs(i-1, j) + dfs(i+1, j) + dfs(i, j+1) + dfs(i, j-1)
 
-        visited.add((i, j))
-        return dfs(i + 1, j) + dfs(i, j + 1) + dfs(i - 1, j) + dfs(i, j - 1)
-
-    for i in range(row):
-        for j in range(col):
-            if grid[i][j] == 1:
-                return dfs(i, j)
+    for x in range(row):
+        for y in range(col):
+            if grid[x][y] == 1:
+                return dfs(x, y)
