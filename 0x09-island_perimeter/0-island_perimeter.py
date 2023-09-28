a0bbf5261f4804b7ebc0941f1f5d286e5790179b
8srael <kouassiisrael36@gmail.com>
2023-09-28 18:38:57 +0000

    Task 0 . 🆗

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -2,36 +2,24 @@
 
 """ Task 0. Island Perimeter """
 
-
-def dfs(grid, i, j, visited):
-    """ Depth First Search"""
-    row = len(grid)
-    col = len(grid[0])
-
-    if i < 0 or j < 0 or i >= row or j >= col or grid[i][j] == 0:
-        return 1
-
-    if (i, j) in visited:
-        return 0
-
-    visited.add((i, j))
-    return (
-        dfs(grid, i + 1, j, visited) +
-        dfs(grid, i, j + 1, visited) +
-        dfs(grid, i - 1, j, visited) +
-        dfs(grid, i, j - 1, visited)
-    )
-
-
 def island_perimeter(grid):
     """
         Returns the perimeter of the island described in grid
     """
-    visited = set()
+    visited  = set()
     row = len(grid)
     col = len(grid[0])
+
+    def dfs(i, j):
+        if i < 0 or j < 0 or i >= row or j >= col or grid[i][j] == 0:
+            return 1
+        if (i, j) in visited:
+            return 0
+
+        visited.add((i, j))
+        return dfs(i + 1, j) + dfs(i, j + 1) + dfs(i - 1, j) + dfs(i, j - 1)
 
     for i in range(row):
         for j in range(col):
             if(grid[i][j] == 1):
-                return dfs(grid, i, j, visited)
+                return dfs(i, j)
