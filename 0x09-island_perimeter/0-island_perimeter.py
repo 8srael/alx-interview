b61fde211f92503ee3763c95ec82e856ab282b5f
8srael <kouassiisrael36@gmail.com>
2023-09-28 17:47:05 +0000

    add task 0. island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+""" Task 0. Island Perimeter """
+
+def island_perimeter(grid):
+    """
+        Returns the perimeter of the island described in grid
+    """
+    visited  = set()
+    row = len(grid)
+    col = len(grid[0])
+    print(f"Size : {(row, col)}")
+
+    def dfs(i, j):
+        if i < 0 or j < 0 or i >= row or j >= col or grid[i][j] == 0:
+            return 1
+
+        if (i, j) in visited:
+            return 0
+
+        visited.add((i, j))
+        return dfs(i + 1, j) + dfs(i, j + 1) + dfs(i - 1, j) + dfs(i, j - 1)
+
+    for i in range(row):
+        for j in range(col):
+            if(grid[i][j] == 1):
+                return dfs(i, j) 
