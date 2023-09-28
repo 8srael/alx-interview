89623b1ea45ae43d054d504756f2e0e1289a1586
8srael <kouassiisrael36@gmail.com>
2023-09-28 18:26:23 +0000

    Task 0.🧹

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,33 +3,33 @@
 """ Task 0. Island Perimeter """
 
 
+def dfs(grid, i, j, visited):
+    """ Depth First Search"""
+    row = len(grid)
+    col = len(grid[0])
+
+    if i < 0 or j < 0 or i >= row or j >= col or grid[i][j] == 0:
+        return 1
+
+    if (i, j) in visited:
+        return 0
+
+    visited.add((i, j))
+    return dfs(grid, i + 1, j, visited) + \
+           dfs(grid, i, j + 1, visited) + \
+           dfs(grid, i - 1, j, visited) + \
+           dfs(grid, i, j - 1, visited)
+
+
 def island_perimeter(grid):
     """
         Returns the perimeter of the island described in grid
     """
-    if type(grid) is not list:
-        return 0
-
     visited = set()
     row = len(grid)
     col = len(grid[0])
-    perimeter = 0
-
-    def dfs(i, j):
-        if i < 0 or j < 0 or i >= row or j >= col or grid[i][j] == 0:
-            return 1
-
-        if (i, j) in visited:
-            return 0
-
-        visited.add((i, j))
-        perimeter = dfs(i + 1, j)
-        perimeter += dfs(i, j + 1)
-        perimeter += dfs(i - 1, j)
-        perimeter += dfs(i, j - 1)
-        return perimeter
 
     for i in range(row):
         for j in range(col):
             if(grid[i][j] == 1):
-                return dfs(i, j)
+                return dfs(grid, i, j, visited)
