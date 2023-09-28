f896601a886dddf3ae721c1465bd9a19a23efc0b
8srael <kouassiisrael36@gmail.com>
2023-09-28 19:39:55 +0000

    ok

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -9,18 +9,18 @@ def island_perimeter(grid):
     """
     row = len(grid)
     col = len(grid[0])
-        
+
     visited = [[0 for x in range(col)] for y in range(row)]
-        
+ 
     def dfs(i, j):
         """Depth first search method"""
         if i < 0 or i > row-1 or j < 0 or j > col-1 or grid[i][j] == 0:
             return 1
-        
+
         if visited[i][j]:
             return 0
         visited[i][j] = 1
-        
+
         return dfs(i-1, j) + dfs(i+1, j) + dfs(i, j+1) + dfs(i, j-1)
 
     for x in range(row):
