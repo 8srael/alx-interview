1959be65b47f8933655e84445ffc9223134e6c96
8srael <kouassiisrael36@gmail.com>
2023-09-29 06:31:14 +0000

    Task 0.✅

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,22 +3,24 @@
 
 
 def island_perimeter(grid):
-    """Computes the perimeter of an island with no lakes.
     """
-    perimeter = 0
-    if type(grid) != list:
-        return 0
-    n = len(grid)
-    for i, row in enumerate(grid):
-        m = len(row)
-        for j, cell in enumerate(row):
-            if cell == 0:
-                continue
-            edges = (
-                i == 0 or (len(grid[i - 1]) > j and grid[i - 1][j] == 0),
-                j == m - 1 or (m > j + 1 and row[j + 1] == 0),
-                i == n - 1 or (len(grid[i + 1]) > j and grid[i + 1][j] == 0),
-                j == 0 or row[j - 1] == 0,
-            )
-            perimeter += sum(edges)
-    return perimeter
+        Returns the perimeter of the island described in grid
+    """
+    visited = set()
+    row = len(grid)
+    col = len(grid[0])
+
+    def dfs(i, j):
+        """depth first search method recursive method"""
+        if i < 0 or j < 0 or i >= row or j >= col or grid[i][j] == 0:
+            return 1
+        if (i, j) in visited:
+            return 0
+
+        visited.add((i, j))
+        return dfs(i + 1, j) + dfs(i, j + 1) + dfs(i - 1, j) + dfs(i, j - 1)
+
+    for i in range(row):
+        for j in range(col):
+            if grid[i][j] == 1:
+                return dfs(i, j)
